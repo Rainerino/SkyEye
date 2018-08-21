1c7325f5576d03e0df96cdc9cb194e8350e4fa13
Rainerino <albertyanyy@gmail.com>
2018-08-21 02:50:06 -0700

    no virtul

--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
-virtualenv -p /usr/bin/python3 ./venv && \
-    source ./venv/bin/activate && \
-    pip install -U -r requirements.txt && \
-    deactivate
+sudo apt-get install python-pygame
+pip install -U -r requirements.txt
