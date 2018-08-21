7f3e703af0921781cda85a4bb38172cefb5006ca
Rainerino <albertyanyy@gmail.com>
2018-08-21 02:58:30 -0700

    added debug

--- sky_eye.py
+++ sky_eye.py
@@ -4,6 +4,8 @@ import os
 from GeoTag import geotag
 import time
 
+DEBUG = True
+
 
 def main():
 	path_output_dir = "./image"
@@ -21,21 +23,22 @@ def main():
 			except Exception as e:
 				print(e)
 
-	try:
-		vehicle = dronekit.connect("/dev/ttyUSB0", wait_ready=True, baud=1500000)
+	if not DEBUG:
+		try:
+			vehicle = dronekit.connect("/dev/ttyUSB0", wait_ready=True, baud=1500000)
 
-		while not vehicle.home_location:
-			cmds = vehicle.commands
-			cmds.download()
-			cmds.wait_ready()
-			if not vehicle.home_location:
-				print(" Waiting for home location ...")
-		# We have a home location, so print it!
-		print("\n Home location: %s" % vehicle.home_location)
-	except:
-		print("Cannot connect!")
-		import sys
-		sys.exit(1)
+			while not vehicle.home_location:
+				cmds = vehicle.commands
+				cmds.download()
+				cmds.wait_ready()
+				if not vehicle.home_location:
+					print(" Waiting for home location ...")
+			# We have a home location, so print it!
+			print("\n Home location: %s" % vehicle.home_location)
+		except:
+			print("Cannot connect!")
+			import sys
+			sys.exit(1)
 
 	vidcap = cv2.VideoCapture(0)
 
@@ -46,13 +49,21 @@ def main():
 		if success:
 			current_path = os.path.join(path_output_dir, '%d.JPEG') % count
 			cv2.imwrite(current_path, image)
-			lat =vehicle.location.global_relative_frame.lat
-			lon = vehicle.location.global_relative_frame.lon
-			alt = vehicle.location.global_relative_frame.alt
-			if alt < 0:
-				alt = 0.15
-			hdg = vehicle.heading
-			print("Current location is at: lat: %f, lon: %f, alt %f, hdg %f" %( lat, lon, alt, hdg))
+
+			if DEBUG:
+				lat = 1
+				lon = 1
+				alt = 1
+				hdg = 1
+			else:
+				lat = vehicle.location.global_relative_frame.lat
+				lon = vehicle.location.global_relative_frame.lon
+				alt = vehicle.location.global_relative_frame.alt
+				if alt < 0:
+					alt = 0.15
+				hdg = vehicle.heading
+
+			print("Current location is at: lat: %f, lon: %f, alt %f, hdg %f" % (lat, lon, alt, hdg))
 			geotag.write_geo_tag(current_path, lat, lon, alt, hdg)
 
 			count += 1
