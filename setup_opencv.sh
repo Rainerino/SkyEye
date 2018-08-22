a7d857c3ec501b3506777a6cd3390eb5217cba4e
Rainerino <albertyanyy@gmail.com>
2018-08-22 00:50:09 -0700

    stdlib error fix?

--- setup_opencv.sh
+++ setup_opencv.sh
@@ -22,12 +22,12 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo rpi-update
 
-sudo apt-get install build-essential git cmake pkg-config
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libgtk2.0-dev
-sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install -y build-essential git cmake pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk2.0-dev
+sudo apt-get install -y libatlas-base-dev gfortran
 cd ~
 git clone https://github.com/Itseez/opencv.git
 cd opencv
@@ -37,7 +37,7 @@ git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv_contrib
 git checkout 3.1.0
 
-sudo apt-get install python3-dev
+sudo apt-get install -y python3-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 pip install numpy
@@ -49,6 +49,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D INSTALL_C_EXAMPLES=OFF \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D ENABLE_PRECOMPILED_HEADERS=OFF \
     -D BUILD_EXAMPLES=ON ..
 make -j4
 sudo make install
